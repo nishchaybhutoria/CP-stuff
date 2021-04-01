fb839ecd1bb073ddbd69bcc384fdf337f150f17d
Nishchay Bhutoria <43699361+nishchaybhutoria@users.noreply.github.com>
2021-04-01 13:09:02 +0530

    Update template.cpp

--- miscellaneous/template.cpp
+++ miscellaneous/template.cpp
@@ -1,10 +1,13 @@
+// #pragma GCC optimize("Ofast,unroll-loops")
+// #pragma GCC target("avx,avx2,sse,sse2")
+ 
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
-
+ 
 using namespace __gnu_pbds;
 using namespace std;
-
+ 
 #define ll long long
 #define ld long double
 #define all(x) x.begin(), x.end()
@@ -15,41 +18,36 @@ using namespace std;
 #define S second
 #define mp make_pair
 #define random mt19937 rng(chrono::steady_clock::now().time_since_epoch().count())
+ 
+template <typename T> using oset = tree <pair <T, T>, null_type, less <pair <T, T>>, rb_tree_tag, tree_order_statistics_node_update>;
 
 template <typename Container>
-void print(Container& container)
-{
-    auto Start = container.begin(), End = container.end();
+void Print(Container& container, int starting) {
+    auto Start = container.begin() + starting, End = container.end();
     while (Start != End) cout << *(Start++) << " ";
     cout << '\n';
 }
-
-template <typename T> using oset = tree <pair <T, T>, null_type, less <pair <T, T>>, rb_tree_tag, tree_order_statistics_node_update>;
-
+ 
 template <typename T>
 void print(T&& t) {cout << t << '\n';}
-
+ 
 template <typename T, typename... Args>
-void print(T&& t, Args&&... args)
-{
+void print(T&& t, Args&&... args) {
     cout << t << " ";
     print(forward<Args>(args)...);
 }
 
-inline void solve()
-{
-
+inline void solve() {
+    
 }
-
-int32_t main()
-{
+ 
+int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
     int T = 1;
     // cin >> T;
-    for (int test = 1; test <= T; ++test)
-    {
+    for (int test = 1; test <= T; ++test) {
         // cout << "Case #" << test << ": ";
         solve();
     }
