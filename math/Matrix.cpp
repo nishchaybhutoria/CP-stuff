a32aa499b10e237868ab2fa59e5609d7e3e9c973
Nishchay Bhutoria <43699361+nishchaybhutoria@users.noreply.github.com>
2021-04-10 14:58:04 +0530

    Update Matrix.cpp

--- math/Matrix.cpp
+++ math/Matrix.cpp
@@ -1,36 +1,36 @@
 const long long MOD = 1e9 + 7;
 
 struct Matrix {
-     vector <vector <int>> mat;
-     int rows, cols;
+    vector <vector <int>> mat;
+    int rows, cols;
      
-     Matrix() {}
+    Matrix() {}
      
-     Matrix(vector <vector <int>> vals) : mat(vals), rows((int)vals.size()), cols((int)vals.front().size()) {}
+    Matrix(vector <vector <int>> vals) : mat(vals), rows((int)vals.size()), cols((int)vals.front().size()) {}
      
-     static Matrix identity(int n) {
-         vector <vector <int>> vals(n, vector <int> (n));
-         for (int i = 0; i < n; ++i) {
-             vals[i][i] = 1;
-         }
-         return vals;
-     }
+    static Matrix identity(int n) {
+        vector <vector <int>> vals(n, vector <int> (n));
+        for (int i = 0; i < n; ++i) {
+            vals[i][i] = 1;
+        }
+        return vals;
+    }
      
-     Matrix operator * (const Matrix& other) const {
-         int n = rows, m = other.cols;
-         vector <vector <int>> res(rows, vector <int> (cols));
-         for (int i = 0; i < n; ++i) {
-             for (int j = 0; j < m; ++j) {
-                 long long temp = 0;
-                 for (int k = 0; k < cols; ++k) {
-                     temp += (mat[i][k] * 1LL * other.mat[k][j]);
-                     // if (temp >= MOD) temp %= MOD;
-                 }
-                 res[i][j] = temp;
-             }
-         }
-         return move(Matrix(move(res)));
-     }
+    Matrix operator * (const Matrix& other) const {
+        int n = rows, m = other.cols;
+        vector <vector <int>> res(rows, vector <int> (cols));
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                long long temp = 0;
+                for (int k = 0; k < cols; ++k) {
+                    temp += (mat[i][k] * 1LL * other.mat[k][j]);
+                    // if (temp >= MOD) temp %= MOD;
+                }
+                res[i][j] = temp;
+            }
+        }
+        return move(Matrix(move(res)));
+    }
 };
 
 Matrix power(Matrix a, ll p) {
